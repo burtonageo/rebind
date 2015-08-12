2799a3db0032d993757e40dd0ebcba5a0be9ced4
George Burton <burtonageo@gmail.com>
2015-08-12 09:40:50 +0100

    Add rebind interface/implementation (apart from converting to an InputTranslator)

--- src/rebind.rs
+++ src/rebind.rs
@@ -1,6 +1,88 @@
-use {Action, ButtonTuple};
+use {Action, ButtonTuple, MouseTranslationData};
+use piston_window::Size;
 use std::collections::HashMap;
+use std::convert::Into;
+use std::default::Default;
 
 pub struct InputRebind<A: Action> {
-    keymap: HashMap<A, ButtonTuple>
+    keymap: HashMap<A, ButtonTuple>,
+    mouse_data: MouseTranslationData
+}
+
+impl<A: Action> InputRebind<A> {
+    pub fn new(size: Size) -> Self {
+        InputRebind {
+            keymap: HashMap::new(),
+            mouse_data: MouseTranslationData::new(size)
+        }
+    }
+
+    pub fn insert_action(&mut self, action: A) -> Option<ButtonTuple> {
+        self.keymap.insert(action, ButtonTuple::new())
+    }
+
+    pub fn insert_action_with_buttons(&mut self, action: A, buttons: ButtonTuple) -> Option<ButtonTuple> {
+        self.keymap.insert(action, buttons)
+    }
+
+    pub fn get_bindings(&self, action: &A) -> Option<&ButtonTuple> {
+        self.keymap.get(action)
+    }
+
+    pub fn get_bindings_mut(&mut self, action: &mut A) -> Option<&mut ButtonTuple> {
+        self.keymap.get_mut(action)
+    }
+
+    pub fn get_x_scroll_inverted(&self) -> &bool {
+        &self.mouse_data.x_axis_scroll_inverted
+    }
+
+    pub fn get_x_scroll_inverted_mut(&mut self) -> &mut bool {
+        &mut self.mouse_data.x_axis_scroll_inverted
+    }
+
+    pub fn get_y_scroll_inverted(&self) -> &bool {
+        &self.mouse_data.y_axis_scroll_inverted
+    }
+
+    pub fn get_y_scroll_inverted_mut(&mut self) -> &mut bool {
+        &mut self.mouse_data.y_axis_scroll_inverted
+    }
+
+    pub fn get_x_motion_inverted(&self) -> &bool {
+        &self.mouse_data.x_axis_motion_inverted
+    }
+
+    pub fn get_x_motion_inverted_mut(&mut self) -> &mut bool {
+        &mut self.mouse_data.x_axis_motion_inverted
+    }
+
+    pub fn get_y_motion_inverted(&self) -> &bool {
+        &self.mouse_data.y_axis_motion_inverted
+    }
+
+    pub fn get_y_motion_inverted_mut(&mut self) -> &mut bool {
+        &mut self.mouse_data.y_axis_motion_inverted
+    }
+
+    pub fn get_viewport_size(&self) -> &Size {
+        &self.mouse_data.viewport_size
+    }
+
+    pub fn get_viewport_size_mut(&mut self) -> &mut Size {
+        &mut self.mouse_data.viewport_size
+    }
+}
+
+impl<A: Action> Default for InputRebind<A> {
+    fn default() -> Self {
+        InputRebind::new((800, 600).into())
+    }
+}
+
+#[cfg(unimplemented)]
+impl<A: Action> Into<InputTranslator<A>> for InputRebind<A> {
+    fn into(self) -> InputTranslator<A> {
+        InputTranslator::new(/* etc.. */)
+    }
 }
