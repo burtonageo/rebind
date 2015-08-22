e85f617803de9991c846907e67d2bcf2ac201666
George Burton <burtonageo@gmail.com>
2015-08-22 18:27:01 +0100

    Add body for test_conversion_from_rebind_to_translator, and some supporting functions/scaffolding. Test fails

--- src/test.rs
+++ src/test.rs
@@ -3,7 +3,7 @@
 use {Action, InputTranslator, RebindBuilder, InputRebind};
 use window::Size;
 
-#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 enum TestAction {
     Action1, Action2, Action3, Action4, Action5,
     Action6, Action7, Action8, Action9, Action10
@@ -20,6 +20,21 @@ const TEST_SIZE: Size = Size {
     height: 600
 };
 
+fn create_prepopulated_builder() -> TestBuilder {
+    use input::keyboard::Key;
+    use input::Button::Keyboard;
+
+    RebindBuilder::new(TEST_SIZE)
+        .with_action_mapping(Keyboard(Key::Up),    TestAction::Action1)
+        .with_action_mapping(Keyboard(Key::W),     TestAction::Action1)
+        .with_action_mapping(Keyboard(Key::Down),  TestAction::Action2)
+        .with_action_mapping(Keyboard(Key::S),     TestAction::Action2)
+        .with_action_mapping(Keyboard(Key::Left),  TestAction::Action3)
+        .with_action_mapping(Keyboard(Key::A),     TestAction::Action3)
+        .with_action_mapping(Keyboard(Key::Right), TestAction::Action4)
+        .with_action_mapping(Keyboard(Key::D),     TestAction::Action4)
+}
+
 #[test]
 fn test_empty() {
     let builder = TestBuilder::new(TEST_SIZE).build();
@@ -27,5 +42,12 @@ fn test_empty() {
 
 #[test]
 fn test_conversion_from_rebind_to_translator() {
-    assert!(true)
+    let translator: TestTranslator = create_prepopulated_builder().build();
+    let translator_clone = translator.clone();
+    let converted_translator: TestTranslator = {
+        let rebind: TestRebind = translator.into();
+        rebind.into()
+    };
+
+    assert_eq!(converted_translator, translator_clone);
 }
